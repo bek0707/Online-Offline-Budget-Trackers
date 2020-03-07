23a01183f5dbf03190197b8e5aa5ba3677f221f6
bek <bekhruz0707@gmail.com>
2020-03-06 19:36:26 -0500

    homework

---
+++ routes/api.js
@@ -0,0 +1,43 @@
+const router = require("express").Router();
+var path = require('path');
+const Transaction = require("../models/transaction.js");
+
+router.post("/api/transaction", ({ body }, res) => {
+  Transaction.create(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.post("/api/transaction/bulk", ({ body }, res) => {
+  Transaction.insertMany(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/transaction", (req, res) => {
+  Transaction.find({})
+    .sort({ date: -1 })
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+
+router.get('/',function(req,res) {
+  res.sendFile(path.join(__dirname, "../public/assets/index.html"));
+
+});
+
+module.exports = router;
+
